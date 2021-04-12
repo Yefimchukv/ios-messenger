7435f88f1f32157d42d03a2544741ec904aa950a
Vitaliy Yefimchuk <fry2000.3000@gmail.com>
2021-04-12 10:58:25 +0300

    Create navigation and prepare MVC

---
+++ Messenger/Sources/Controllers/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  Messenger
+//
+//  Created by Виталий Ефимчук on 11.04.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
